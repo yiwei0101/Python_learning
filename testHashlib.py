45d85950ab0c2f9d06b60e0b826e3705aa52c563
yiwei0101 <yiwei0101@gmail.com>
2021-09-03 14:24:03 +0800

    inter lambda

--- testHashlib.py
+++ testHashlib.py
@@ -4,10 +4,13 @@ import hashlib
 def main():
     digester = hashlib.md5()
     with open('requirements.txt', 'rb') as file_stream:
-        data = file_stream.read(1034)
-        while data:
+        # data = file_stream.read(1034)
+        # while data:
+        #     digester.update(data)
+        #     data = file_stream.read(10 24)
+        file_iter = iter(lambda: file_stream.read(1024), b'')
+        for data in file_iter:
             digester.update(data)
-            data = file_stream.read(1024)
         print(digester.hexdigest())
 
 
